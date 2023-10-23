e7164ae7e300a3c422f54f397523d9e33769a037
Vladimir Kosarev <painkiller_1999@mail.ru>
2023-10-23 19:29:16 +0300

    testCommit

--- src/components/react.dev.learn/ReactingToInputWithState.jsx
+++ src/components/react.dev.learn/ReactingToInputWithState.jsx
@@ -11,7 +11,7 @@ export default function ReactingToInputWithState() {
 
         alert(result); // "готово!"
 
-        console.log(`qdasdasdasdas`);
+        console.log(`qdasdasdasdasadasdads`);
     }
 
     console.log(f());
